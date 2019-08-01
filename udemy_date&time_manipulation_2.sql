b3592eae8f7c0cb275e1d6f85a6313fd59a6cbee
Michal Borkowski <44318554+mike1806@users.noreply.github.com>
2019-08-01 21:30:07 +0100

    Add files via upload

---
+++ udemy_date&time_manipulation_2.sql
@@ -0,0 +1,27 @@
+CREATE TABLE people (name VARCHAR(100), birthdate DATE, birthtime TIME, birthdt DATETIME);
+ 
+INSERT INTO people (name, birthdate, birthtime, birthdt)
+VALUES('Padma', '1983-11-11', '10:07:35', '1983-11-11 10:07:35');
+ 
+INSERT INTO people (name, birthdate, birthtime, birthdt)
+VALUES('Larry', '1943-12-25', '04:10:42', '1943-12-25 04:10:42');
+ 
+SELECT * FROM people;
+ 
+SELECT DATEDIFF(NOW(), birthdate) FROM people;
+ 
+SELECT name, birthdate, DATEDIFF(NOW(), birthdate) FROM people;
+ 
+SELECT birthdt FROM people;
+ 
+SELECT birthdt, DATE_ADD(birthdt, INTERVAL 1 MONTH) FROM people;
+ 
+SELECT birthdt, DATE_ADD(birthdt, INTERVAL 10 SECOND) FROM people;
+ 
+SELECT birthdt, DATE_ADD(birthdt, INTERVAL 3 QUARTER) FROM people;
+ 
+SELECT birthdt, birthdt + INTERVAL 1 MONTH FROM people;
+ 
+SELECT birthdt, birthdt - INTERVAL 5 MONTH FROM people;
+ 
+SELECT birthdt, birthdt + INTERVAL 15 MONTH + INTERVAL 10 HOUR FROM people;

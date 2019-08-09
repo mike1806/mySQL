fa1333259f84efa140159513ba612657f80f442d
Michal Borkowski <44318554+mike1806@users.noreply.github.com>
2019-08-09 22:54:34 +0100

    Add files via upload

---
+++ udemy_book-database_log_operators_4.sql
@@ -0,0 +1,92 @@
+CREATE TABLE books 
+	(
+		book_id INT NOT NULL AUTO_INCREMENT,
+		title VARCHAR(100),
+		author_fname VARCHAR(100),
+		author_lname VARCHAR(100),
+		released_year INT,
+		stock_quantity INT,
+		pages INT,
+		PRIMARY KEY(book_id)
+	);
+
+INSERT INTO books (title, author_fname, author_lname, released_year, stock_quantity, pages)
+VALUES
+('The Namesake', 'Jhumpa', 'Lahiri', 2003, 32, 291),
+('Norse Mythology', 'Neil', 'Gaiman',2016, 43, 304),
+('American Gods', 'Neil', 'Gaiman', 2001, 12, 465),
+('Interpreter of Maladies', 'Jhumpa', 'Lahiri', 1996, 97, 198),
+('A Hologram for the King: A Novel', 'Dave', 'Eggers', 2012, 154, 352),
+('The Circle', 'Dave', 'Eggers', 2013, 26, 504),
+('The Amazing Adventures of Kavalier & Clay', 'Michael', 'Chabon', 2000, 68, 634),
+('Just Kids', 'Patti', 'Smith', 2010, 55, 304),
+('A Heartbreaking Work of Staggering Genius', 'Dave', 'Eggers', 2001, 104, 437),
+('Coraline', 'Neil', 'Gaiman', 2003, 100, 208),
+('What We Talk About When We Talk About Love: Stories', 'Raymond', 'Carver', 1981, 23, 176),
+("Where I'm Calling From: Selected Stories", 'Raymond', 'Carver', 1989, 12, 526),
+('White Noise', 'Don', 'DeLillo', 1985, 49, 320),
+('Cannery Row', 'John', 'Steinbeck', 1945, 95, 181),
+('Oblivion: Stories', 'David', 'Foster Wallace', 2004, 172, 329),
+('Consider the Lobster', 'David', 'Foster Wallace', 2005, 92, 343);
+
+# data manipulation 	
+
+SELECT 10 != 10;
+-- false
+ 
+SELECT 15 > 14 && 99 - 5 <= 94;
+-- true
+ 
+SELECT 1 IN (5,3) || 9 BETWEEN 8 AND 10;
+-- true
+ 
+SELECT title, released_year FROM books WHERE released_year < 1980;
+ 
+SELECT title, author_lname FROM books WHERE author_lname='Eggers' OR author_lname='Chabon';
+ 
+SELECT title, author_lname FROM books WHERE author_lname IN ('Eggers','Chabon');
+ 
+SELECT title, author_lname, released_year FROM books WHERE author_lname = 'Lahiri' && released_year > 2000;
+ 
+SELECT title, pages FROM books WHERE pages >= 100 && pages <=200;
+ 
+SELECT title, pages FROM books WHERE pages BETWEEN 100 AND 200;
+ 
+SELECT 
+    title, 
+    author_lname 
+FROM books 
+WHERE 
+    author_lname LIKE 'C%' OR 
+    author_lname LIKE 'S%';
+ 
+SELECT 
+    title, 
+    author_lname 
+FROM books 
+WHERE 
+    SUBSTR(author_lname,1,1) = 'C' OR 
+    SUBSTR(author_lname,1,1) = 'S';
+ 
+SELECT title, author_lname FROM books 
+WHERE SUBSTR(author_lname,1,1) IN ('C', 'S');
+ 
+SELECT 
+    title, 
+    author_lname,
+    CASE
+        WHEN title LIKE '%stories%' THEN 'Short Stories'
+        WHEN title = 'Just Kids' OR title = 'A Heartbreaking Work of Staggering Genius' THEN 'Memoir'
+        ELSE 'Novel'
+    END AS TYPE
+FROM books;
+ 
+SELECT author_fname, author_lname,
+    CASE 
+        WHEN COUNT(*) = 1 THEN '1 book'
+        ELSE CONCAT(COUNT(*), ' books')
+    END AS COUNT
+FROM books 
+GROUP BY author_lname, author_fname;
+
+
